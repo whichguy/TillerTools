e6b91ee50ea6a00b7d89ca5f6f814905bf693d23
Jim Wiese <jim@fortifiedscience.org`>
2024-10-06 09:34:26 -0700

    renamed main to layout

--- sidebar/menu.js
+++ sidebar/menu.js
@@ -1,13 +1,13 @@
 function openLoggingSidebar() {
   const html = HtmlService.createTemplateFromFile('sidebar/sidebar').evaluate()
-    .setTitle('Logging Sidebar')
+    .setTitle('Tiller Sidebar')
     .setWidth(300);
   SpreadsheetApp.getUi().showSidebar(html);
 }
 function onOpen() {
   var ui = SpreadsheetApp.getUi();
   // Create a custom menu in the Google Sheets UI.
-  ui.createMenu('Logger')
-      .addItem('Launch', 'openLoggingSidebar')
+  ui.createMenu('Tiller Sidebar')
+      .addItem('Open...', 'openLoggingSidebar')
       .addToUi();
 }
